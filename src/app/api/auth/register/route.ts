0e3317b86fca88c4b36f4c2f9e21bf0bf0351ce7
cleiva <cleivaj93@gmail.com>
2025-01-08 14:31:22 +0000

    fixingVercel

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,26 +2,26 @@ import { prisma } from "@/libs/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from 'bcrypt';
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
     try {
         const data = await request.json();
-        //verify all data is here
+        // Verify all data is here
         if (!data || !data.email || !data.password || !data.name || !data.role) {
             return NextResponse.json(
                 { error: "Missing required fields" },
                 { status: 400 }
             );
         }
-        //verify the email does not exist in dB
+        // Verify the email does not exist in the database
         const existUser = await prisma.user.findUnique({
-            where: { email: data.email}
-        })
-        if(existUser){
-            return NextResponse.json({message: 'User already exist'}, { status: 400 })
+            where: { email: data.email }
+        });
+        if (existUser) {
+            return NextResponse.json({ message: 'User already exists' }, { status: 400 });
         }
-        //hash password with bcrypt
+        // Hash password with bcrypt
         const hashedPassword = await bcrypt.hash(data.password, 10);
-        //create a newUser
+        // Create a new user
         const newUser = await prisma.user.create({
             data: {
                 email: data.email,
@@ -31,21 +31,21 @@ export async function POST(request: NextRequest){
                 passwdRecovery: "",
             }
         });
-        //hide a passwd to send it to front
-        const { password: _, ...user } = newUser;
+        // Destructure the password without assigning it to a variable
+        const { password, ...user } = newUser;
         return NextResponse.json(
             { message: "User created successfully", user: user },
             { status: 201 }
         );
     } catch (error) {
         console.error('Error completo:', error);
-        
+
         return NextResponse.json(
-            { 
+            {
                 error: "Error creating user",
                 details: error instanceof Error ? error.message : 'Unknown error'
             },
             { status: 500 }
         );
-    }    
+    }
 }
