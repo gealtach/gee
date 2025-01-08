dbf6fe05ce7b6ce6faef7477ca2dc3795e60461f
cleiva <cleivaj93@gmail.com>
2025-01-08 15:35:53 +0000

    conectado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 import Login from "./components/login";
-import Register from "./components/register";
 
 export default function Home() {
   return (
       <div className="flex flex-col items-center">
         <h1>GEE - Estrutura de Dados e Cálculos Automáticos</h1>
         <Login />
-        <Register/>
       </div>
   );
 }
